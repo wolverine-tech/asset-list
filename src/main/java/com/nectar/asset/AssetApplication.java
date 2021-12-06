b843d460779b2bb839004c3ca3c1cf7ee84e81e4
natasha <74241430+natasharomanoff25@users.noreply.github.com>
2021-12-06 17:54:53 +0400

    changes

--- src/main/java/com/nectar/asset/AssetApplication.java
+++ src/main/java/com/nectar/asset/AssetApplication.java
@@ -1,6 +1,7 @@
 package com.nectar.asset;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nectar.asset.ServiceImpl.AssetServiceImpl;
+import com.nectar.thing.beans.ThingDataBean;
 
 @SpringBootApplication
 @ComponentScan({ "com.nectar" })
@@ -27,7 +30,8 @@ public class AssetApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		assetServiceImpl.getAssetLatest(Collections.singletonList("Emaar Square Building 3"),Collections.singletonList("FireElectricalPump"));
+		List<ThingDataBean> thingdatabean = assetServiceImpl.getAssetLatest(Collections.singletonList("b3bbc402-33fb-4894-ae80-c6d865ed30c2"),Collections.singletonList("FireElectricalPump"));
+		System.out.println(new ObjectMapper().writeValueAsString(thingdatabean));
 	}
 
 }
--- src/main/java/com/nectar/asset/ServiceImpl/AssetServiceImpl.java
+++ src/main/java/com/nectar/asset/ServiceImpl/AssetServiceImpl.java
@@ -1,7 +1,6 @@
 package com.nectar.asset.ServiceImpl;
 
-
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +8,7 @@ import java.util.stream.Collectors;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +16,42 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.nectar.honeycomb.solr.db.util.SolrUtil;
 import com.nectar.thing.beans.ThingDataBean;
 
 @Service
 public class AssetServiceImpl {
-	
-	public  final String SEARCH_TAG_IDS = "searchTagIds";
-	public  final String TYPE = "type";
-	
-	private  Logger LOGGER = LoggerFactory.getLogger(AssetServiceImpl.class);
-	
+
+	public final String SEARCH_TAG_IDS = "searchTagIds";
+	public final String TYPE = "type";
+
+	private Logger LOGGER = LoggerFactory.getLogger(AssetServiceImpl.class);
+
 	@Value("${honeycomb.asset.latest.solr.collection}")
-	private  String solrCollecton;
+	private String solrCollecton;
 
 	@Value("${honeycomb.asset.solr.latest.fields}")
-	private  String assetLatestsolrFields;
-
+	private String assetLatestsolrFields;
 
 	@Autowired
 	SolrUtil solrUtil;
-	
-	
-	public  List<ThingDataBean> getAssetLatest(List<String> path,List<String> type) {
+
+	public List<ThingDataBean> getAssetLatest(List<String> path, List<String> type) {
 		Map<String, String> queryFields = new HashMap<String, String>();
 		Map<String, String> filterFields = new HashMap<String, String>();
 		setQueryFields(SEARCH_TAG_IDS, path, queryFields);
-		setFilterFields(TYPE,type,filterFields);
+		setFilterFields(TYPE, type, filterFields);
 		SolrQuery solrQuery = new SolrQuery();
 
 		solrQuery = setSolrQuery(queryFields, filterFields);
-		
 
 		return getAssetLatestResponse(solrQuery);
 	}
-	
-	private  Map<String, String> setFilterFields(String field, List<String> sources,
-			Map<String, String> filterFields) {
+
+	private Map<String, String> setFilterFields(String field, List<String> sources, Map<String, String> filterFields) {
 
 		if (!CollectionUtils.isEmpty(sources)) {
 
@@ -61,8 +60,8 @@ public class AssetServiceImpl {
 		return filterFields;
 
 	}
-	
-	private   Map<String, String> setQueryFields(String field, List<String> sources, Map<String, String> queryFields) {
+
+	private Map<String, String> setQueryFields(String field, List<String> sources, Map<String, String> queryFields) {
 
 		if (!CollectionUtils.isEmpty(sources)) {
 
@@ -72,8 +71,7 @@ public class AssetServiceImpl {
 
 	}
 
-	
-	private  SolrQuery setSolrQuery(Map<String, String> queryFields, Map<String, String> filterFields) {
+	private SolrQuery setSolrQuery(Map<String, String> queryFields, Map<String, String> filterFields) {
 
 		SolrQuery solrQuery = new SolrQuery();
 
@@ -97,24 +95,30 @@ public class AssetServiceImpl {
 		return solrQuery;
 
 	}
-	
-	public  List<ThingDataBean> getAssetLatestResponse(SolrQuery solrQuery) {
+
+	public List<ThingDataBean> getAssetLatestResponse(SolrQuery solrQuery) {
 		LOGGER.debug("solr query :{}", solrQuery.toString());
 		QueryResponse response = solrUtil.querySolr(solrCollecton, solrQuery);
 		List<ThingDataBean> assetLatest = null;
 		if (response.getResults().size() > 0) {
+			Gson gson = new Gson();
+			final SolrDocumentList documents = response.getResults();
+
+			String json = gson.toJson(response.getResults());
+		
+			List<ThingDataBean> assetFilterList = new ArrayList<ThingDataBean>();
 			try {
-				assetLatest = response.getBeans(ThingDataBean.class);
+				assetFilterList = new ObjectMapper().readValue(json, new TypeReference<List<ThingDataBean>>() {
+				});
 			} catch (Exception e) {
-				LOGGER.error("Error in mapping {}", e);
-				return null;
+				LOGGER.error("Error in mapping");
 			}
 		}
 
 		else {
 			LOGGER.warn("No response forund from solr");
 		}
-		return assetLatest;
+		return assetFilterList;
 
 	}
 
